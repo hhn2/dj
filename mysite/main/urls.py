dbe4991d77778b639a99eae93b8c4d1433e24aae
Hannah Hwang <hwanghaena@gmail.com>
2024-01-21 22:15:25 -0500

    Tech with Tim #2

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("v1/", views.v1, name="view1"),
+    path("<str:name>", views.index, name="index"),
+
 ]
