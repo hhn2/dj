e4a217b6cdcee3aaced4da8d4418352f0d17ac39
Hannah Hwang <hwanghaena@gmail.com>
2024-01-27 13:11:01 -0500

    no message

--- mysite/register/forms.py
+++ mysite/register/forms.py
@@ -1,6 +1,12 @@
-from dhjankgo.contrib.auth import login, authenticat4e
-from django.contri.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class RegisterForm(UserCreationForm):
-    email = models.EmailField()
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
