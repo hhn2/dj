b973d2013639ea950c156a0fa8cdb988a0925e3c
Hannah Hwang <hwanghaena@gmail.com>
2024-01-28 19:38:57 -0500

    tech with tim done

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -42,9 +42,14 @@ def create(response):
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
+            response.user.todolist.add(t)
             
         return HttpResponseRedirect("/%i" %t.id)
     else:
         form = CreateNewList()
+        
     return render(response, "main/create.html", {"form": form})
 
+
+def view(response):
+    return render(response, "main/view.html", {})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,11 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = "/" 
+LOGOUT_REDIRECT_URL = "/"
